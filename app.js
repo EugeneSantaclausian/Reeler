fb7adc194bdaa89a9162e26083227f191dfb4e59
EugeneSantaclausian <eugenedumoga@gmail.com>
2020-12-26 09:14:18 +0000

    "Initial Commit on Master"

---
+++ app.js
@@ -0,0 +1,76 @@
+//import axios from "axios";
+document.getElementById("allmovies").style.display = "none";
+document.getElementById("moviesHeader").style.display = "none";
+document.getElementById("closeMovies").style.display = "none";
+
+const url = "http://localhost:2876/api/movies";
+const getMovies = (path) => {
+  axios.get(path).then(
+    (response) => {
+      var result = response.data;
+      console.log(result);
+      return show(result);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+
+const genre_url = "http://localhost:2876/api/movies";
+const getGenres = (path) => {
+  axios.get(path).then(
+    (response) => {
+      var result = response.data;
+      console.log(result);
+      return show(result);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+
+function show(data) {
+  const item = `
+      <a href="#" class="list-group-item list-group-item-action" >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1" id="title"></h5>
+          <small id="year"></small>
+        </div>
+        <small id="genre"></small>
+      </a>
+  `;
+
+  itemList = data.map(
+    (movie) => `
+  <a href="#" class="list-group-item list-group-item-action" >
+  <div class="d-flex w-100 justify-content-between">
+    <h5 class="mb-1" id="title">${movie.title}</h5>
+    <small id="year">${movie.year}</small>
+  </div>
+  <small id="genre">${movie.genre}</small>
+</a>
+  `
+  );
+
+  const arrangedList = itemList.join("");
+
+  // Setting innerHTML as tab variable
+  document.getElementById("allmovies").style.display = "block";
+  document.getElementById("moviesHeader").style.display = "block";
+  document.getElementById("closeMovies").style.display = "block";
+  document.getElementById("subHeadText").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("dropdownMenuButton").style.display = "none";
+  document.getElementById("allmovies").innerHTML = arrangedList;
+  return;
+}
+
+const closeMoviesList = () => {
+  //e.preventDefault();
+  document.getElementById("allmovies").style.display = "none";
+  document.getElementById("moviesHeader").style.display = "none";
+  document.getElementById("closeMovies").style.display = "none";
+  return;
+};
---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 2876;
+
+app.use(cors());
+
+const movies = [
+  { id: 1, title: "Jungle Book", genre: "Animation", year: 2003 },
+  { id: 2, title: "Justice League", genre: "Action", year: 2016 },
+  { id: 3, title: "Home Alone", genre: "Comedy", year: 1996 },
+  {
+    id: 4,
+    title: "Harry Potter & The Socerer's Stone",
+    genre: "Mystery",
+    year: 2007,
+  },
+  { id: 5, title: "Lion King", genre: "Animation", year: 2004 },
+  { id: 6, title: "Avengers End Game", genre: "Action", year: 2018 },
+  { id: 7, title: "The Devil Wears Prada", genre: "Comedy", year: 2007 },
+  {
+    id: 4,
+    title: "Chronicles of Narnia",
+    genre: "Mystery",
+    year: 2012,
+  },
+];
+
+//Get request for movies per the genre
+app.get("/api/movies/:genre", (req, res) => {
+  movie = movies.filter((mov) => mov.genre == req.params.genre);
+  movie == null || undefined
+    ? res.send("Genre Not Found!!").status(404)
+    : res.send(movie);
+});
+
+app.get("/api/movies", (req, res) => {
+  res.send(movies);
+});
+
+app.listen(port, () => console.log(`Listening on Port ${port}....`));
