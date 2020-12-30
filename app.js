78fd3fa236f960d6e1fb77f88e363d23b42b5353
EugeneSantaclausian <eugenedumoga@gmail.com>
2020-12-30 16:05:02 +0000

    "Cliet Requests Updates"

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ document.getElementById("moviesHeader").style.display = "none";
 document.getElementById("closeMovies").style.display = "none";
 document.getElementById("spinner").style.display = "none";
 
+//------Request to GET All Movies------
 const url = "http://localhost:2876/api/movies";
-const getMovies = (path) => {
+const requestMovies = (path) => {
   axios.get(path).then(
     (response) => {
       var result = response.data;
       console.log(result);
+      document.getElementById("spinner").style.display = "none";
+      document.getElementById("moviesHeader").innerHTML = "All Movies";
       return show(result);
     },
     (error) => {
@@ -19,26 +22,125 @@ const getMovies = (path) => {
   return;
 };
 
-const anime_url = `http://localhost:2876/api/movies/Animation`;
-const getAnimationMovies = () => {
-  document.getElementById("spinner").display = "block";
+const getMovies = () => {
+  document.getElementById("subHeadText").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("dropdownMenuButton").style.display = "none";
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(() => requestMovies(url), 1000);
+  return;
+};
+//--------------------------------------------
 
+//-------Request to GET All Animation Movies------------
+const anime_url = `http://localhost:2876/api/movies/Animation`;
+const requestAnime = () => {
   axios.get(anime_url).then(
     (response) => {
       var result = response.data;
       console.log(result);
+      document.getElementById("spinner").style.display = "none";
       document.getElementById("moviesHeader").innerHTML = "Animation Movies";
-      document.getElementById("spinner").display = "none";
       return show(result);
     },
     (error) => {
       console.log(error);
     }
   );
-
-  return;
 };
 
+const getAnimationMovies = () => {
+  document.getElementById("subHeadText").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("dropdownMenuButton").style.display = "none";
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(() => requestAnime(), 1000);
+  return;
+};
+//-------------------------------------------------------
+
+//-------Request to GET All Action Movies------------
+const action_url = `http://localhost:2876/api/movies/Action`;
+const requestAction = () => {
+  axios.get(action_url).then(
+    (response) => {
+      var result = response.data;
+      console.log(result);
+      document.getElementById("spinner").style.display = "none";
+      document.getElementById("moviesHeader").innerHTML = "Action Movies";
+      return show(result);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+
+const getActionMovies = () => {
+  document.getElementById("subHeadText").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("dropdownMenuButton").style.display = "none";
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(() => requestAction(), 1000);
+  return;
+};
+//-------------------------------------------------------
+
+//-------Request to GET All Comedy Movies------------
+const comedy_url = `http://localhost:2876/api/movies/Comedy`;
+const requestComedy = () => {
+  axios.get(comedy_url).then(
+    (response) => {
+      var result = response.data;
+      console.log(result);
+      document.getElementById("spinner").style.display = "none";
+      document.getElementById("moviesHeader").innerHTML = "Comedy Movies";
+      return show(result);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+
+const getComedyMovies = () => {
+  document.getElementById("subHeadText").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("dropdownMenuButton").style.display = "none";
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(() => requestComedy(), 1000);
+  return;
+};
+//-------------------------------------------------------
+
+//-------Request to GET All Mystery Movies------------
+const mystery_url = `http://localhost:2876/api/movies/Mystery`;
+const requestMystery = () => {
+  axios.get(mystery_url).then(
+    (response) => {
+      var result = response.data;
+      console.log(result);
+      document.getElementById("spinner").style.display = "none";
+      document.getElementById("moviesHeader").innerHTML = "Mystery Movies";
+      return show(result);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+
+const getMysteryMovies = () => {
+  document.getElementById("subHeadText").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("dropdownMenuButton").style.display = "none";
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(() => requestMystery(), 1000);
+  return;
+};
+//-------------------------------------------------------
+
+//----------Function to Display Data-------------------------
 function show(data) {
   const item = `
       <a href="#" class="list-group-item list-group-item-action" >
@@ -74,9 +176,10 @@ function show(data) {
   document.getElementById("allmovies").innerHTML = arrangedList;
   return;
 }
+//-------------------------------------------------------
 
+//------Close Button--------------------------------------------
 const closeMoviesList = () => {
-  //e.preventDefault();
   document.getElementById("allmovies").style.display = "none";
   document.getElementById("moviesHeader").style.display = "none";
   document.getElementById("closeMovies").style.display = "none";
@@ -85,3 +188,4 @@ const closeMoviesList = () => {
   document.getElementById("dropdownMenuButton").style.display = "inline";
   return;
 };
+//-------------------------------------------------------------
