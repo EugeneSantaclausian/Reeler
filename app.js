925c14556c979a0e7db03c00033363c26ac7ff29
EugeneSantaclausian <eugenedumoga@gmail.com>
2021-01-06 00:51:13 +0000

    "Create New Movie Update"

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
 document.getElementById("allmovies").style.display = "none";
 document.getElementById("moviesHeader").style.display = "none";
 document.getElementById("closeMovies").style.display = "none";
+document.getElementById("closeForm").style.display = "none";
 document.getElementById("spinner").style.display = "none";
+document.getElementById("movieForm").style.display = "none";
+document.getElementById("movieModal").style.display = "none";
 
 //------Request to GET All Movies------
-const url = "https://reeler.netlify.app/.netlify/functions/index/api/movies";
+const prod_url =
+  "https://reeler.netlify.app/.netlify/functions/index/api/movies"; //production url
+const dev_url = "http://localhost:9000/.netlify/functions/index/api/movies"; //development url
 const requestMovies = (path) => {
   axios.get(path).then(
     (response) => {
@@ -27,7 +32,7 @@ const getMovies = () => {
   document.getElementById("buttons").style.display = "none";
   document.getElementById("dropdownMenuButton").style.display = "none";
   document.getElementById("spinner").style.display = "block";
-  setTimeout(() => requestMovies(url), 1000);
+  setTimeout(() => requestMovies(dev_url), 1000);
   return;
 };
 //--------------------------------------------
@@ -144,6 +149,52 @@ const getMysteryMovies = () => {
 };
 //-------------------------------------------------------
 
+//-------Request to Submit New Movie(POST REQUEST)------------
+
+//Show The Form to Receive Movie Details
+const showForm = () => {
+  document.getElementById("subHeadText").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("dropdownMenuButton").style.display = "none";
+  document.getElementById("movieForm").style.display = "block";
+  document.getElementById("closeForm").style.display = "block";
+  //document.getElementById("pageTitle").style.display = "none";
+  return;
+};
+
+const getForm = () => {
+  const mtitle = document.getElementById("movieTitle").value;
+  const myear = document.getElementById("movieYear").value;
+  const mgenre = document.getElementById("selectedGenre").value;
+  //return alert(`${title}, ${year}, ${genre}`);
+  axios
+    .post(
+      prod_url,
+      {
+        title: mtitle,
+        year: myear,
+        genre: mgenre,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then(
+      (response) => {
+        console.log(response.data);
+        document.getElementById("moviesHeader").innerHTML = "New Movie Added!";
+        return showModal(response.data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  return document.getElementById("movieForm").reset();
+};
+//-------------------------------------------------------
+
 //----------Function to Display Data-------------------------
 function show(data) {
   const item = `
@@ -180,6 +231,32 @@ function show(data) {
   document.getElementById("allmovies").innerHTML = arrangedList;
   return;
 }
+
+//--------------SHOW MODAL-----------------------------------------
+
+function showModal(data) {
+  newMovie = `
+  <a href="#" class="list-group-item list-group-item-action" >
+  <div class="d-flex w-100 justify-content-between">
+    <h5 class="mb-1" id="title">${data.title}</h5>
+    <small id="year">${data.year}</small>
+  </div>
+  <small id="genre">${data.genre}</small>
+</a>
+  `;
+
+  //const arrangedList = newMovie.join("");
+
+  // Setting innerHTML as tab variable
+  document.getElementById("allmovies").style.display = "block";
+  document.getElementById("moviesHeader").style.display = "block";
+  document.getElementById("subHeadText").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("dropdownMenuButton").style.display = "none";
+  document.getElementById("movieForm").style.display = "none";
+  document.getElementById("allmovies").innerHTML = newMovie;
+  return;
+}
 //-------------------------------------------------------
 
 //------Close Button--------------------------------------------
@@ -190,6 +267,10 @@ const closeMoviesList = () => {
   document.getElementById("subHeadText").style.display = "block";
   document.getElementById("buttons").style.display = "block";
   document.getElementById("dropdownMenuButton").style.display = "inline";
+  document.getElementById("movieForm").style.display = "none";
+  document.getElementById("closeForm").style.display = "none";
+  document.getElementById("pageTitle").style.display = "block";
+  document.getElementById("movieForm").reset();
   return;
 };
 //-------------------------------------------------------------
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
 const cors = require("cors");
+const Joi = require("joi");
 //const port = process.env.PORT || 2876;
 
 app.use(cors());
-app.use("/.netlify/functions/index", router);
+app.use("/.netlify/functions/index", router); //Router to use Netlify Functions
+router.use(express.urlencoded({ extended: true })); //Body Parser
+router.use(express.json()); //Body Parser
 
 const movies = [
   { id: 1, title: "Jungle Book", genre: "Animation", year: 2003 },
@@ -39,6 +43,36 @@ router.get("/api/movies/:genre", (req, res) => {
 
 router.get("/api/movies", (req, res) => {
   res.send(movies);
+});
+
+//Post Request for  A New Movie
+router.post("/api/movies", (req, res) => {
+  //Joi Schema for Input Validation
+  const schema = Joi.object({
+    title: Joi.string().max(30).required(),
+    genre: Joi.string().max(30).required(),
+    year: Joi.number().min(3).max(4).required(),
+  });
+
+  try {
+    const result = schema.validateAsync(req.body);
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+
+  //Movie Object from Request Body
+  const movie = {
+    id: movies.length + 1,
+    title: req.body.title,
+    genre: req.body.genre,
+    year: req.body.year,
+  };
+  //Push movie to Movies
+  movies.unshift(movie);
+  console.log("REQ.BODY:", req.body);
+  //Return Response to Client
+  res.send(movie);
 });
 
 //app.listen(port, () => console.log(`Listening on Port ${port}....`));
