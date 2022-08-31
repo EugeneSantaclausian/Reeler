307f1d5e7fc01b67692fc607dedf348365c389c6
Eugene Dumoga <eugenedumoga@gmail.com>
2022-08-31 21:18:55 +0000

    Additional HTML Files To Test Payment

---
+++ test.js
@@ -0,0 +1,23 @@
+const pay = async () => {
+  let axiosConfig = {
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Origin": "https://posapi.usebillbox.com/",
+      appId: "20ff1ee5-db8d-35d8-41f0-bc7bc9026179",
+    },
+  };
+
+  axios
+    .post(
+      "https://reeler.herokuapp.com/api/movies/test-payment",
+      {
+        requestId: "37c99dfa-3afb-4127-b280-0078a1b28d2d",
+        appReference: "bargainmoto",
+        secret: "Bargain1Moto",
+      },
+      axiosConfig
+    )
+    .then((response) => {
+      console.log(response);
+    });
+};
