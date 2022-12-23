4d4838b68dd9f0dd6e0b5fa45d3419b5a3c77cb8
Eugene Dumoga <eugenedumoga@gmail.com>
2022-12-23 22:40:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3500;
 var corsOptions = {
   origin: [port, "https://reeler.netlify.app"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["Content-Type"],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions)); //add corsOptions as an argument in cors() when deploying to netlify
